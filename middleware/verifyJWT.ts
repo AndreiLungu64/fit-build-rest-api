c75965ea08d994a7ead764b4c49a1f76bbc4f8e5
AndreiLungu64 <andreilungu777@yahoo.ro>
2025-03-25 15:27:32 +0200

    Added comments in verifyJWT.ts

--- middleware/verifyJWT.ts
+++ middleware/verifyJWT.ts
@@ -10,24 +10,31 @@ config();
 //   }
 // }
 
+//redeffine the Request parameter to contain the user property (which it doesn't by default)
 interface RequestWithUser extends Request {
   user?: string | JwtPayload;
 }
 
 const verifyJWT = (req: RequestWithUser, res: Response, next: NextFunction) => {
+  //accessing the bearer token (access token) from the authorisation header (a request has multiple headers authorisation contianing the bearer token)
   const authHeader = req.headers["authorization"];
   if (!authHeader) {
-    res.sendStatus(401); //unauthorised
+    res.sendStatus(401); //unauthorised, it means an unauthorized user tried to access a protected route
     return;
   }
 
   console.log(authHeader); //will print : "Beare" token (bearer string and the actual token)
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1]; //get only the token
+
+  //checks if the token is valid by verifying its signature using the access token secret
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (err, decoded) => {
     if (err || !decoded) {
-      res.sendStatus(403); //forbidden, invalid token
+      res.sendStatus(403); //forbidden, invalid token, or error
       return;
     }
+
+    //Extracting the username from the decoded JWT payload and attaching it to the request object
+    /*The purpose is to make the authenticated user's identity available to all subsequent middleware and route handlers that process this request. Instead of decoding the token in multiple places, you decode it once in the middleware, and then any route handler can simply use req.user to know which user is making the request.*/
     req.user = (decoded as JwtPayload).username;
     next();
   });
